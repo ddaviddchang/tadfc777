232de0a58391775d09d01424b3f7686045a97fb2
Tomik Aghajanian <taghajanian@cpp.edu>
2017-10-09 17:38:29 -0700

    added HangmanGameUI.java

---
+++ CS245QuarterProject/src/cs245quarterproject/src/PointAndClickGame.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs245quarterproject.src;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.*;
+import java.util.Random;
+
+/**
+ *
+ * @author Tomik
+ */
+public class PointAndClickGame extends javax.swing.JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+      HangmanGameUI hman = new HangmanGameUI();
+    }
+
+}
